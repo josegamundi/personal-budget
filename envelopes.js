f06f8eb231b761c46314f2bb957a5fba51a91cef
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2025-04-18 10:49:41 -0400

    Change the status code from 200 to 201, when an envelope is created

--- envelopes.js
+++ envelopes.js
@@ -11,7 +11,7 @@ envelopesRouter.post('/', (req, res, next) => {
     const newEnvelope = req.body;
     try {
         createEnvelope(budget, newEnvelope);
-        res.status(200).send(budget);
+        res.status(201).send(budget);
     } catch(error) {
         res.status(400).send(error.message);
     }
