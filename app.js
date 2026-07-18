0688551c11cb175263cb9c9ae925c423783d1711
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-07-18 11:59:14 -0400

    Establish the default error handler

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
-
 import { transactionsRouter } from './routes/transactions.js';
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json()); // for parsing application/json
+// For parsing application/json
+app.use(express.json());
 
+// Routers
 app.use('/transactions', transactionsRouter);
 
+// Default error handler
+app.use((err, req, res, next) => {
+  const errorCode = err.status || 500;
+  const errorMessage = err.message;
+  
+  res.status(errorCode);
+  res.json({
+    "Error Code" : errorCode,
+    "Error message" : errorMessage
+  });
+});
+
+// Listening port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Personal Budget App listening on port ${port}`);
 });
