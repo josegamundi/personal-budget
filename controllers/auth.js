1307643ba5250fecb6fb05a00fcbe463478a5802
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-07-26 17:21:48 -0400

    Add a user registration controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,28 @@
 import { pool } from "../config/db.js";
+import bcrypt from "bcrypt";
 
-export const registerUser = async () => {};
-export const loginUser = async () => {};
+export const registerUser = async (req, res, next) => {
+  try {
+    const user = req.body;
+    user.password = await bcrypt.hash(user.password, 10);
+    const query = {
+      text: `
+        INSERT INTO users(email, password)
+        VALUES($1, $2)
+        RETURNING user_id, email, created_at
+      `,
+      values: [ 
+        user.email, 
+        user.password
+      ]
+    };
+    const response = await pool.query(query);
+    res.json(response.rows);
+  } catch(error) {
+    next(error);
+  }
+};
+
+export const loginUser = async (req, res, next) => {
+
+};
