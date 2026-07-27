340fe249b545c4cd415dfb465d1d995e7cfa72cf
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-07-27 16:23:14 -0400

    Add a token authentication controller

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import jwt from "jsonwebtoken";
+
+export const tokenAuth = async (req, res, next) => {
+  let token = req.get('Authorization');
+  if (!token) return res.status(401).json({ "error": "Invalid token, access denied" });
+  token = token.split(' ')[1];
+  
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch(error) {
+    res.status(401).json({ "error": error.message })
+  }
+}
