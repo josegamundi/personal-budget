f51986406aea1859ff0716402c21a942e5e4f117
Jose Gamundi <46800189+josegamundi@users.noreply.github.com>
2026-08-11 12:02:57 -0400

    Update the categories table

--- personal_budget.sql
+++ personal_budget.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
 
 CREATE TABLE IF NOT EXISTS categories (
   category_id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL UNIQUE
+  name TEXT NOT NULL,
+  user_id INTEGER NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+  UNIQUE (name, user_id)
 );
 
 CREATE TYPE transaction_type AS ENUM ('income', 'expense');
